6f0821170742b7271de333a854f77f2cc273c025
Brandon Beck <bmbeck@gmail.com>
2020-12-02 23:34:51 -0600

    2020/03

---
+++ cmd/2020/03-1/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bbeck/advent-of-code/aoc"
+)
+
+func main() {
+	m, width, height := InputToMap(2020, 03)
+
+	p := aoc.Point2D{}
+	dx := 3
+	dy := 1
+
+	var count int
+	for p.Y <= height {
+		if m[p] {
+			count++
+		}
+
+		p = aoc.Point2D{X: (p.X + dx) % (width + 1), Y: p.Y + dy}
+	}
+
+	fmt.Println(count)
+}
+
+func Dump(m map[aoc.Point2D]bool, width, height int, current aoc.Point2D) {
+	for y := 0; y <= height; y++ {
+		for x := 0; x <= width; x++ {
+			p := aoc.Point2D{X: x, Y: y}
+			if p == current {
+				fmt.Print("*")
+				continue
+			}
+			if m[p] {
+				fmt.Print("#")
+			} else {
+				fmt.Print(".")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func InputToMap(year, day int) (map[aoc.Point2D]bool, int, int) {
+	m := make(map[aoc.Point2D]bool)
+	var width, height int
+	for y, line := range aoc.InputToLines(year, day) {
+		height = y
+		for x, c := range line {
+			m[aoc.Point2D{X: x, Y: y}] = c == '#'
+			width = x
+		}
+	}
+	return m, width, height
+}
---
+++ cmd/2020/03-2/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bbeck/advent-of-code/aoc"
+)
+
+func main() {
+	m, width, height := InputToMap(2020, 03)
+
+	product := CountTrees(m, width, height, 1, 1) *
+		CountTrees(m, width, height, 3, 1) *
+		CountTrees(m, width, height, 5, 1) *
+		CountTrees(m, width, height, 7, 1) *
+		CountTrees(m, width, height, 1, 2)
+
+	fmt.Println(product)
+}
+
+func CountTrees(m map[aoc.Point2D]int, width, height int, dx, dy int) int {
+	next := func(p aoc.Point2D) aoc.Point2D {
+		return aoc.Point2D{X: (p.X + dx) % (width + 1), Y: p.Y + dy}
+	}
+
+	origin := func() aoc.Point2D {
+		return aoc.Point2D{}
+	}
+
+	var count int
+	for p := origin(); p.Y <= height; p = next(p) {
+		count += m[p]
+	}
+
+	return count
+}
+
+func InputToMap(year, day int) (map[aoc.Point2D]int, int, int) {
+	m := make(map[aoc.Point2D]int)
+	var width, height int
+	for y, line := range aoc.InputToLines(year, day) {
+		height = y
+		for x, c := range line {
+			if c == '#' {
+				m[aoc.Point2D{X: x, Y: y}] = 1
+			}
+			width = x
+		}
+	}
+	return m, width, height
+}
