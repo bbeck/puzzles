ff4b7bed90323b02e939dd156c5ab74d0f7b0c77
Brandon Beck <bmbeck@gmail.com>
2020-12-12 14:02:02 -0600

    2020/12

---
+++ cmd/2020/12-1/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bbeck/advent-of-code/aoc"
+)
+
+func main() {
+	location := aoc.Point2D{X: 0, Y: 0}
+	direction := aoc.Point2D{X: 1, Y: 0}
+
+	for _, instruction := range InputToInstructions(2020, 12) {
+		switch {
+		case instruction.action == "N":
+			location.Y -= instruction.value
+
+		case instruction.action == "S":
+			location.Y += instruction.value
+
+		case instruction.action == "E":
+			location.X += instruction.value
+
+		case instruction.action == "W":
+			location.X -= instruction.value
+
+		case instruction.action == "L":
+			direction = RotateClockwise(direction, 360-instruction.value)
+
+		case instruction.action == "R":
+			direction = RotateClockwise(direction, instruction.value)
+
+		case instruction.action == "F":
+			location = aoc.Point2D{
+				X: location.X + direction.X*instruction.value,
+				Y: location.Y + direction.Y*instruction.value,
+			}
+		}
+	}
+
+	fmt.Println(location.ManhattanDistance(aoc.Point2D{}))
+}
+
+func RotateClockwise(point aoc.Point2D, theta int) aoc.Point2D {
+	// Clockwise rotation about the origin
+	//   x' = x*cos(theta) - y*sin(theta)
+	//   y' = y*cos(theta) + x*sin(theta)
+	//
+	//   theta  sin(theta)  cos(theta)
+	//       0          0           1
+	//      90          1           0
+	//     180          0          -1
+	//     270         -1           0
+	sin := map[int]int{90: 1, 180: 0, 270: -1}
+	cos := map[int]int{90: 0, 180: -1, 270: 0}
+
+	return aoc.Point2D{
+		X: point.X*cos[theta] - point.Y*sin[theta],
+		Y: point.Y*cos[theta] + point.X*sin[theta],
+	}
+}
+
+type Instruction struct {
+	action string
+	value  int
+}
+
+func InputToInstructions(year, day int) []Instruction {
+	var instructions []Instruction
+	for _, line := range aoc.InputToLines(year, day) {
+		instructions = append(instructions, Instruction{
+			action: string(line[0]),
+			value:  aoc.ParseInt(line[1:]),
+		})
+	}
+
+	return instructions
+}
---
+++ cmd/2020/12-2/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bbeck/advent-of-code/aoc"
+)
+
+func main() {
+	ship := aoc.Point2D{X: 0, Y: 0}
+	waypoint := aoc.Point2D{X: ship.X + 10, Y: ship.Y - 1}
+
+	for _, instruction := range InputToInstructions(2020, 12) {
+		switch instruction.action {
+		case "N":
+			waypoint = aoc.Point2D{X: waypoint.X, Y: waypoint.Y - instruction.value}
+
+		case "S":
+			waypoint = aoc.Point2D{X: waypoint.X, Y: waypoint.Y + instruction.value}
+
+		case "E":
+			waypoint = aoc.Point2D{X: waypoint.X + instruction.value, Y: waypoint.Y}
+
+		case "W":
+			waypoint = aoc.Point2D{X: waypoint.X - instruction.value, Y: waypoint.Y}
+
+		case "L":
+			waypoint = RotateClockwise(waypoint, ship, 360-instruction.value)
+
+		case "R":
+			waypoint = RotateClockwise(waypoint, ship, instruction.value)
+
+		case "F":
+			dx := waypoint.X - ship.X
+			dy := waypoint.Y - ship.Y
+			ship = aoc.Point2D{X: ship.X + instruction.value*dx, Y: ship.Y + instruction.value*dy}
+			waypoint = aoc.Point2D{X: ship.X + dx, Y: ship.Y + dy}
+		}
+	}
+
+	fmt.Println(ship.ManhattanDistance(aoc.Point2D{}))
+}
+
+func RotateClockwise(point aoc.Point2D, origin aoc.Point2D, theta int) aoc.Point2D {
+	// Clockwise rotation about the origin
+	//   x' = x*cos(theta) - y*sin(theta)
+	//   y' = y*cos(theta) + x*sin(theta)
+	//
+	//   theta  sin(theta)  cos(theta)
+	//       0          0           1
+	//      90          1           0
+	//     180          0          -1
+	//     270         -1           0
+	sin := map[int]int{90: 1, 180: 0, 270: -1}
+	cos := map[int]int{90: 0, 180: -1, 270: 0}
+
+	return aoc.Point2D{
+		X: (point.X-origin.X)*cos[theta] - (point.Y-origin.Y)*sin[theta] + origin.X,
+		Y: (point.Y-origin.Y)*cos[theta] + (point.X-origin.X)*sin[theta] + origin.Y,
+	}
+}
+
+type Instruction struct {
+	action string
+	value  int
+}
+
+func InputToInstructions(year, day int) []Instruction {
+	var instructions []Instruction
+	for _, line := range aoc.InputToLines(year, day) {
+		instructions = append(instructions, Instruction{
+			action: string(line[0]),
+			value:  aoc.ParseInt(line[1:]),
+		})
+	}
+
+	return instructions
+}
