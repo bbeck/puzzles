d0a78e4a9ca88029a6a378224bbd027a8f950d94
Brandon Beck <bmbeck@gmail.com>
2020-12-26 09:46:40 -0600

    Refactor handheld CPU into 2020/08 since that's the only day that uses it.

--- cmd/2020/08-1/main.go
+++ cmd/2020/08-1/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/bbeck/advent-of-code/aoc/cpus"
+	"github.com/bbeck/advent-of-code/aoc"
 )
 
 func main() {
-	program := cpus.InputToHandheldProgram(2020, 8)
+	program := InputToHandheldProgram(2020, 8)
 	seen := make(map[int]bool)
 
-	var cpu cpus.HandheldCPU
+	var cpu HandheldCPU
 	for {
 		if seen[cpu.IP] {
 			break
@@ -21,4 +22,59 @@ func main() {
 	}
 
 	fmt.Println(cpu.ACC)
+}
+
+type HandheldCPU struct {
+	IP     int
+	ACC    int
+	Halted bool
+}
+
+func (cpu *HandheldCPU) RunProgram(program []HandheldInstruction) {
+	for !cpu.Halted {
+		cpu.Step(program)
+	}
+}
+
+func (cpu *HandheldCPU) Step(program []HandheldInstruction) {
+	if cpu.IP < 0 || cpu.IP >= len(program) {
+		cpu.Halted = true
+		return
+	}
+
+	instruction := program[cpu.IP]
+	switch instruction.Op {
+	case "acc":
+		cpu.ACC += instruction.Argument
+		cpu.IP++
+	case "jmp":
+		cpu.IP += instruction.Argument
+	case "nop":
+		cpu.IP++
+	}
+}
+
+type HandheldInstruction struct {
+	Op       string
+	Argument int
+}
+
+func InputToHandheldProgram(year, day int) []HandheldInstruction {
+	var instructions []HandheldInstruction
+	for _, line := range aoc.InputToLines(year, day) {
+		var op string
+		var argument int
+
+		_, err := fmt.Sscanf(line, "%s %d", &op, &argument)
+		if err != nil {
+			log.Fatalf("unable to parse line: %s", line)
+		}
+
+		instructions = append(instructions, HandheldInstruction{
+			Op:       op,
+			Argument: argument,
+		})
+	}
+
+	return instructions
 }
--- cmd/2020/08-2/main.go
+++ cmd/2020/08-2/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/bbeck/advent-of-code/aoc/cpus"
+	"github.com/bbeck/advent-of-code/aoc"
 )
 
 func main() {
-	program := cpus.InputToHandheldProgram(2020, 8)
+	program := InputToHandheldProgram(2020, 8)
 
 	// Loop through each instruction of the program considering what happens if it
 	// were changed from a jmp -> nop or nop -> jmp.
@@ -38,10 +39,10 @@ func main() {
 	fmt.Println(acc)
 }
 
-func IsInfiniteLoop(program []cpus.HandheldInstruction) (bool, int) {
+func IsInfiniteLoop(program []HandheldInstruction) (bool, int) {
 	seen := make(map[int]bool)
 
-	var cpu cpus.HandheldCPU
+	var cpu HandheldCPU
 	for !cpu.Halted {
 		if seen[cpu.IP] {
 			return true, 0
@@ -52,4 +53,59 @@ func IsInfiniteLoop(program []cpus.HandheldInstruction) (bool, int) {
 	}
 
 	return false, cpu.ACC
+}
+
+type HandheldCPU struct {
+	IP     int
+	ACC    int
+	Halted bool
+}
+
+func (cpu *HandheldCPU) RunProgram(program []HandheldInstruction) {
+	for !cpu.Halted {
+		cpu.Step(program)
+	}
+}
+
+func (cpu *HandheldCPU) Step(program []HandheldInstruction) {
+	if cpu.IP < 0 || cpu.IP >= len(program) {
+		cpu.Halted = true
+		return
+	}
+
+	instruction := program[cpu.IP]
+	switch instruction.Op {
+	case "acc":
+		cpu.ACC += instruction.Argument
+		cpu.IP++
+	case "jmp":
+		cpu.IP += instruction.Argument
+	case "nop":
+		cpu.IP++
+	}
+}
+
+type HandheldInstruction struct {
+	Op       string
+	Argument int
+}
+
+func InputToHandheldProgram(year, day int) []HandheldInstruction {
+	var instructions []HandheldInstruction
+	for _, line := range aoc.InputToLines(year, day) {
+		var op string
+		var argument int
+
+		_, err := fmt.Sscanf(line, "%s %d", &op, &argument)
+		if err != nil {
+			log.Fatalf("unable to parse line: %s", line)
+		}
+
+		instructions = append(instructions, HandheldInstruction{
+			Op:       op,
+			Argument: argument,
+		})
+	}
+
+	return instructions
 }
