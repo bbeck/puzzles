0aa2557f351ff7ffc37be4f03c58580a3717474b
Brandon Beck <bmbeck@gmail.com>
2021-12-16 23:33:37 -0600

    2021/17

---
+++ cmd/2021/17-1/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bbeck/advent-of-code/aoc"
+	"sort"
+)
+
+func main() {
+	minX, maxX, minY, maxY := InputToTargetArea()
+
+	var solutions []Solution
+	for vx := -1000; vx <= 1000; vx++ {
+		for vy := -1000; vy <= 1000; vy++ {
+			ok, highest := Simulate(vx, vy, minX, maxX, minY, maxY)
+			if ok {
+				solutions = append(solutions, Solution{vx, vy, highest})
+			}
+		}
+	}
+
+	sort.Slice(solutions, func(i, j int) bool {
+		return solutions[i].hy < solutions[j].hy
+	})
+
+	fmt.Println(solutions[len(solutions)-1])
+}
+
+func Simulate(vx, vy, minX, maxX, minY, maxY int) (bool, int) {
+	hit := func(p Probe) bool {
+		return minX <= p.x && p.x <= maxX && minY <= p.y && p.y <= maxY
+	}
+
+	p := Probe{
+		vx: vx,
+		vy: vy,
+	}
+
+	var highest int
+	for p.y >= minY {
+		highest = aoc.MaxInt(highest, p.y)
+		if hit(p) {
+			return true, highest
+		}
+
+		p.Step()
+	}
+
+	return false, 0
+}
+
+type Solution struct {
+	vx, vy, hy int
+}
+
+type Probe struct {
+	x, y, vx, vy int
+}
+
+func (p *Probe) Step() {
+	p.x += p.vx
+	p.y += p.vy
+
+	if p.vx > 0 {
+		p.vx--
+	} else if p.vx < 0 {
+		p.vx++
+	}
+	p.vy--
+}
+
+func InputToTargetArea() (int, int, int, int) {
+	return 25, 67, -260, -200
+}
