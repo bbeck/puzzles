f2eeff3d2692419e791560f4dc028f5994a5fb9a
Brandon Beck <bmbeck@gmail.com>
2021-12-16 20:55:43 -0600

    Refactor 2021/16

--- cmd/2021/16-1/main.go
+++ cmd/2021/16-1/main.go
@@ -7,146 +7,232 @@ import (
 )
 
 func main() {
-	packet := InputToPacket()
+	bs := InputToBitStream()
+	packet := ReadPacket(bs)
 
 	var walk func(p Packet, fn func(Packet))
 	walk = func(p Packet, fn func(Packet)) {
 		fn(p)
-		for _, c := range p.children {
+		for _, c := range p.Children {
 			walk(c, fn)
 		}
 	}
 
 	var sum int
-	walk(packet, func(p Packet) {
+	walk(*packet, func(p Packet) {
 		sum += p.V
 	})
 	fmt.Println(sum)
 }
 
+func InputToBitStream() *BitStream {
+	input := aoc.InputToString(2021, 16)
+
+	// There's a newline at the end of the file that needs to be stripped
+	input = strings.TrimSpace(input)
+
+	// Convert the input into an array of integers.  This will handle the
+	// conversion of the data from strings of hexadecimal digits to numbers.  In
+	// order to produce data in 8-bit chunks we'll read two digits at a time.
+	var data []uint8
+	for i := 0; i < len(input); i += 2 {
+		n := aoc.ParseIntWithBase(input[i:i+2], 16)
+		data = append(data, uint8(n))
+	}
+
+	return NewBitStream(data)
+}
+
 type Packet struct {
-	kind string
+	Kind string
 	V, T int
 
 	// data for literal packets
-	literal int
+	Value int
 
 	// data for operator packets
-	children []Packet
+	Mode     int
+	Children []Packet
 }
 
-func InputToPacket() Packet {
-	var bits []int
-	for _, c := range strings.TrimSpace(aoc.InputToString(2021, 16)) {
-		n := aoc.ParseIntWithBase(string(c), 16)
-		bs := ToBits(n)
-		bits = append(bits, bs...)
+func (p Packet) String() string {
+	indent := func(n int) string {
+		var sb strings.Builder
+		for i := 0; i < n; i++ {
+			sb.WriteRune(' ')
+		}
+		return sb.String()
 	}
 
-	bits, p := ReadPacket(bits)
-	return p
+	var helper func(ind int, p Packet) string
+	helper = func(ind int, p Packet) string {
+		if p.Kind == "literal" {
+			return indent(ind) + fmt.Sprintf("Literal: %d", p.Value)
+		}
+
+		var sb strings.Builder
+		sb.WriteString(indent(ind) + "Packet{\n")
+		sb.WriteString(indent(ind) + fmt.Sprintf("  Kind: %s\n", p.Kind))
+		sb.WriteString(indent(ind) + fmt.Sprintf("  V: %d\n", p.V))
+		sb.WriteString(indent(ind) + fmt.Sprintf("  T: %d\n", p.T))
+
+		sb.WriteString(indent(ind) + fmt.Sprintf("  Mode: %d\n", p.Mode))
+		sb.WriteString(indent(ind) + "  Children: [\n")
+		for _, c := range p.Children {
+			sb.WriteString(helper(ind+4, c) + "\n")
+		}
+		sb.WriteString(indent(ind) + "  ]\n")
+		sb.WriteString(indent(ind) + "}\n")
+		return sb.String()
+	}
+
+	return helper(0, p)
 }
 
-func ReadPacket(bits []int) ([]int, Packet) {
-	var V, T int
-	bits, V = ReadNBits(bits, 3)
-	bits, T = ReadNBits(bits, 3)
+func ReadPacket(bs *BitStream) *Packet {
+	if bs.Size() < 10 {
+		return nil
+	}
+
+	V := bs.ReadNBits(3)
+	T := bs.ReadNBits(3)
 
 	if T == 4 {
-		return ReadLiteralPacket(bits, V)
-	}
+		// Literal packets contain just an immediate value
+		return &Packet{
+			Kind:  "literal",
+			V:     V,
+			T:     T,
+			Value: bs.ReadVarInt(),
+		}
+	} else {
+		// Operator packets contain an operator and sub-packets
+		var subs []Packet
 
-	return ReadOperatorPacket(bits, V)
+		mode := bs.ReadBit()
+		if mode == 0 {
+			// mode 0: the next 15 bits represent the total length (in bits) of the sub-packets
+			nbits := bs.ReadNBits(15)
+
+			// Setup a sub-bit-stream that contains the next nbits of data
+			sbs := bs.Skip(nbits)
+
+			for {
+				p := ReadPacket(sbs)
+				if p == nil {
+					break
+				}
+
+				subs = append(subs, *p)
+			}
+		} else {
+			// mode 1: the next 11 bits represent the number of sub-packets
+			num := bs.ReadNBits(11)
+
+			for i := 0; i < num; i++ {
+				subs = append(subs, *ReadPacket(bs))
+			}
+		}
+
+		return &Packet{
+			Kind:     "operator",
+			V:        V,
+			T:        T,
+			Mode:     mode,
+			Children: subs,
+		}
+	}
 }
 
-func ReadLiteralPacket(bits []int, V int) ([]int, Packet) {
+type BitStream struct {
+	data []uint8
+	dp   int // which integer in data we're on
+	bp   int // which bit in the current integer we're on
+}
+
+func NewBitStream(data []uint8) *BitStream {
+	return &BitStream{
+		data: data,
+		dp:   0,
+		bp:   7,
+	}
+}
+
+func (bs BitStream) String() string {
+	var sb strings.Builder
+
+	sb.WriteString("data: [")
+	for i, n := range bs.data {
+		sb.WriteString(fmt.Sprintf("%b", n))
+		if i < len(bs.data)-1 {
+			sb.WriteString(" ")
+		}
+	}
+	sb.WriteString("], ")
+	sb.WriteString(fmt.Sprintf("dp: %d, bp: %d", bs.dp, bs.bp))
+
+	return sb.String()
+}
+
+// Size returns the number of bits of data remaining in the stream.
+func (bs BitStream) Size() int {
+	// Number of full bytes of data remaining
+	bytes := len(bs.data) - bs.dp - 1
+
+	// Number of bits remaining in the current byte
+	bits := bs.bp + 1
+
+	return 8*bytes + bits
+}
+
+func (bs *BitStream) ReadBit() int {
+	bit := bs.data[bs.dp] & (1 << bs.bp) >> bs.bp
+	bs.bp--
+	if bs.bp < 0 {
+		bs.bp = 7
+		bs.dp++
+	}
+
+	return int(bit)
+}
+
+func (bs *BitStream) ReadNBits(N int) int {
 	var n int
-	bits, n = ReadVarInt(bits)
-	return bits, Packet{
-		kind:    "literal",
-		V:       V,
-		literal: n,
+	for i := 0; i < N; i++ {
+		n = n<<1 | bs.ReadBit()
 	}
+
+	return n
 }
 
-func ReadVarInt(bits []int) ([]int, int) {
-	var N int
-	var more, n int
+func (bs *BitStream) ReadVarInt() int {
+	var n int
 	for {
-		bits, more = ReadNBits(bits, 1)
-		bits, n = ReadNBits(bits, 4)
-		N = N<<4 | n
+		more := bs.ReadBit() != 0
+		n = n<<4 | bs.ReadNBits(4)
 
-		if more == 0 {
+		if !more {
 			break
 		}
 	}
 
-	return bits, N
-}
-
-func ReadNBits(bits []int, N int) ([]int, int) {
-	var n int
-	for i := 0; i < N; i++ {
-		n = n<<1 | bits[0]
-		bits = bits[1:]
-	}
-	return bits, n
-}
-
-func ReadOperatorPacket(bits []int, V int) ([]int, Packet) {
-	var tid int
-	var children []Packet
-
-	bits, tid = ReadNBits(bits, 1)
-	if tid == 0 {
-		var length int
-		bits, length = ReadNBits(bits, 15)
-
-		sbits := bits[:length]
-		bits = bits[length:]
-
-		for len(sbits) > 6 {
-			var p Packet
-			sbits, p = ReadPacket(sbits)
-			children = append(children, p)
-		}
-	} else {
-		var num int
-		bits, num = ReadNBits(bits, 11)
-
-		var p Packet
-		for i := 0; i < num; i++ {
-			bits, p = ReadPacket(bits)
-			children = append(children, p)
-		}
-	}
-
-	packet := Packet{
-		kind:     "operator",
-		V:        V,
-		children: children,
-	}
-
-	return bits, packet
-}
-
-func LiteralValue(data []int) int {
-	var n int
-	for _, d := range data {
-		n = (n << 4) | d
-	}
 	return n
 }
 
-func ToBits(n int) []int {
-	var bits []int
-	for i := 7; i >= 0; i-- {
-		if n&(1<<i) == 0 {
-			bits = append(bits, 0)
-		} else {
-			bits = append(bits, 1)
-		}
+// Skip will skip over the leading n bits from the bit stream and return the skipped bits
+// as a new bit stream.
+func (bs *BitStream) Skip(size int) *BitStream {
+	var data []uint8
+	for size >= 8 {
+		n := bs.ReadNBits(8)
+		data = append(data, uint8(n))
+		size -= 8
 	}
-	return bits[4:]
+
+	if size > 0 {
+		n := bs.ReadNBits(size) << (8 - size)
+		data = append(data, uint8(n))
+	}
+
+	return NewBitStream(data)
 }
--- cmd/2021/16-2/main.go
+++ cmd/2021/16-2/main.go
@@ -9,29 +9,49 @@ import (
 )
 
 func main() {
-	packet := InputToPacket()
+	bs := InputToBitStream()
+	packet := ReadPacket(bs)
 	fmt.Println(packet.Eval())
 }
 
+func InputToBitStream() *BitStream {
+	input := aoc.InputToString(2021, 16)
+
+	// There's a newline at the end of the file that needs to be stripped
+	input = strings.TrimSpace(input)
+
+	// Convert the input into an array of integers.  This will handle the
+	// conversion of the data from strings of hexadecimal digits to numbers.  In
+	// order to produce data in 8-bit chunks we'll read two digits at a time.
+	var data []uint8
+	for i := 0; i < len(input); i += 2 {
+		n := aoc.ParseIntWithBase(input[i:i+2], 16)
+		data = append(data, uint8(n))
+	}
+
+	return NewBitStream(data)
+}
+
 type Packet struct {
-	kind string
+	Kind string
 	V, T int
 
 	// data for literal packets
-	literal int
+	Value int
 
 	// data for operator packets
-	children []Packet
+	Mode     int
+	Children []Packet
 }
 
 func (p Packet) Eval() int {
-	if p.kind == "literal" {
-		return p.literal
+	if p.Kind == "literal" {
+		return p.Value
 	}
 
 	if p.T == 0 {
 		var sum int
-		for _, c := range p.children {
+		for _, c := range p.Children {
 			sum += c.Eval()
 		}
 		return sum
@@ -39,7 +59,7 @@ func (p Packet) Eval() int {
 
 	if p.T == 1 {
 		var prod = 1
-		for _, c := range p.children {
+		for _, c := range p.Children {
 			prod = prod * c.Eval()
 		}
 		return prod
@@ -47,7 +67,7 @@ func (p Packet) Eval() int {
 
 	if p.T == 2 {
 		var min = math.MaxInt
-		for _, c := range p.children {
+		for _, c := range p.Children {
 			min = aoc.MinInt(min, c.Eval())
 		}
 		return min
@@ -55,28 +75,28 @@ func (p Packet) Eval() int {
 
 	if p.T == 3 {
 		var max = 0
-		for _, c := range p.children {
+		for _, c := range p.Children {
 			max = aoc.MaxInt(max, c.Eval())
 		}
 		return max
 	}
 
 	if p.T == 5 {
-		if p.children[0].Eval() > p.children[1].Eval() {
+		if p.Children[0].Eval() > p.Children[1].Eval() {
 			return 1
 		}
 		return 0
 	}
 
 	if p.T == 6 {
-		if p.children[0].Eval() < p.children[1].Eval() {
+		if p.Children[0].Eval() < p.Children[1].Eval() {
 			return 1
 		}
 		return 0
 	}
 
 	if p.T == 7 {
-		if p.children[0].Eval() == p.children[1].Eval() {
+		if p.Children[0].Eval() == p.Children[1].Eval() {
 			return 1
 		}
 		return 0
@@ -86,119 +106,134 @@ func (p Packet) Eval() int {
 	return 0
 }
 
-func InputToPacket() Packet {
-	var bits []int
-	for _, c := range strings.TrimSpace(aoc.InputToString(2021, 16)) {
-		n := aoc.ParseIntWithBase(string(c), 16)
-		bs := ToBits(n)
-		bits = append(bits, bs...)
+func ReadPacket(bs *BitStream) *Packet {
+	if bs.Size() < 10 {
+		return nil
 	}
 
-	bits, p := ReadPacket(bits)
-	return p
-}
-
-func ReadPacket(bits []int) ([]int, Packet) {
-	var V, T int
-	bits, V = ReadNBits(bits, 3)
-	bits, T = ReadNBits(bits, 3)
+	V := bs.ReadNBits(3)
+	T := bs.ReadNBits(3)
 
 	if T == 4 {
-		return ReadLiteralPacket(bits, V)
-	}
+		// Literal packets contain just an immediate value
+		return &Packet{
+			Kind:  "literal",
+			V:     V,
+			T:     T,
+			Value: bs.ReadVarInt(),
+		}
+	} else {
+		// Operator packets contain an operator and sub-packets
+		var subs []Packet
 
-	return ReadOperatorPacket(bits, T, V)
+		mode := bs.ReadBit()
+		if mode == 0 {
+			// mode 0: the next 15 bits represent the total length (in bits) of the sub-packets
+			nbits := bs.ReadNBits(15)
+
+			// Setup a sub-bit-stream that contains the next nbits of data
+			sbs := bs.Skip(nbits)
+
+			for {
+				p := ReadPacket(sbs)
+				if p == nil {
+					break
+				}
+
+				subs = append(subs, *p)
+			}
+		} else {
+			// mode 1: the next 11 bits represent the number of sub-packets
+			num := bs.ReadNBits(11)
+
+			for i := 0; i < num; i++ {
+				subs = append(subs, *ReadPacket(bs))
+			}
+		}
+
+		return &Packet{
+			Kind:     "operator",
+			V:        V,
+			T:        T,
+			Mode:     mode,
+			Children: subs,
+		}
+	}
 }
 
-func ReadLiteralPacket(bits []int, V int) ([]int, Packet) {
+type BitStream struct {
+	data []uint8
+	dp   int // which integer in data we're on
+	bp   int // which bit in the current integer we're on
+}
+
+func NewBitStream(data []uint8) *BitStream {
+	return &BitStream{
+		data: data,
+		dp:   0,
+		bp:   7,
+	}
+}
+
+// Size returns the number of bits of data remaining in the stream.
+func (bs BitStream) Size() int {
+	// Number of full bytes of data remaining
+	bytes := len(bs.data) - bs.dp - 1
+
+	// Number of bits remaining in the current byte
+	bits := bs.bp + 1
+
+	return 8*bytes + bits
+}
+
+func (bs *BitStream) ReadBit() int {
+	bit := bs.data[bs.dp] & (1 << bs.bp) >> bs.bp
+	bs.bp--
+	if bs.bp < 0 {
+		bs.bp = 7
+		bs.dp++
+	}
+
+	return int(bit)
+}
+
+func (bs *BitStream) ReadNBits(N int) int {
 	var n int
-	bits, n = ReadVarInt(bits)
-	return bits, Packet{
-		kind:    "literal",
-		V:       V,
-		literal: n,
+	for i := 0; i < N; i++ {
+		n = n<<1 | bs.ReadBit()
 	}
+
+	return n
 }
 
-func ReadVarInt(bits []int) ([]int, int) {
-	var N int
-	var more, n int
+func (bs *BitStream) ReadVarInt() int {
+	var n int
 	for {
-		bits, more = ReadNBits(bits, 1)
-		bits, n = ReadNBits(bits, 4)
-		N = N<<4 | n
+		more := bs.ReadBit() != 0
+		n = n<<4 | bs.ReadNBits(4)
 
-		if more == 0 {
+		if !more {
 			break
 		}
 	}
 
-	return bits, N
-}
-
-func ReadNBits(bits []int, N int) ([]int, int) {
-	var n int
-	for i := 0; i < N; i++ {
-		n = n<<1 | bits[0]
-		bits = bits[1:]
-	}
-	return bits, n
-}
-
-func ReadOperatorPacket(bits []int, T int, V int) ([]int, Packet) {
-	var tid int
-	var children []Packet
-
-	bits, tid = ReadNBits(bits, 1)
-	if tid == 0 {
-		var length int
-		bits, length = ReadNBits(bits, 15)
-
-		sbits := bits[:length]
-		bits = bits[length:]
-
-		for len(sbits) > 6 {
-			var p Packet
-			sbits, p = ReadPacket(sbits)
-			children = append(children, p)
-		}
-	} else {
-		var num int
-		bits, num = ReadNBits(bits, 11)
-
-		var p Packet
-		for i := 0; i < num; i++ {
-			bits, p = ReadPacket(bits)
-			children = append(children, p)
-		}
-	}
-
-	packet := Packet{
-		kind:     "operator",
-		V:        V,
-		T:        T,
-		children: children,
-	}
-
-	return bits, packet
-}
-
-func LiteralValue(data []int) int {
-	var n int
-	for _, d := range data {
-		n = (n << 4) | d
-	}
 	return n
 }
 
-func ToBits(n int) []int {
-	var bits []int
-	for i := 7; i >= 0; i-- {
-		if n&(1<<i) == 0 {
-			bits = append(bits, 0)
-		} else {
-			bits = append(bits, 1)
-		}
+// Skip will skip over the leading n bits from the bit stream and return the skipped bits
+// as a new bit stream.
+func (bs *BitStream) Skip(size int) *BitStream {
+	var data []uint8
+	for size >= 8 {
+		n := bs.ReadNBits(8)
+		data = append(data, uint8(n))
+		size -= 8
 	}
-	return bits[4:]
+
+	if size > 0 {
+		n := bs.ReadNBits(size) << (8 - size)
+		data = append(data, uint8(n))
+	}
+
+	return NewBitStream(data)
 }
