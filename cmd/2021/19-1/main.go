a59417267a76048e40ab2b2ac1e4fc5c8bdcccff
Brandon Beck <bmbeck@gmail.com>
2021-12-20 16:51:52 -0600

    Optimize 2021/19

--- cmd/2021/19-1/main.go
+++ cmd/2021/19-1/main.go
@@ -2,87 +2,134 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/bbeck/advent-of-code/aoc"
+	"log"
 )
 
 func main() {
 	scanners := InputToScanners()
-	fmt.Println("num scanners:", len(scanners))
 
-outer:
-	for len(scanners) > 1 {
-		fmt.Println("there are", len(scanners), "scanners remaining")
-		for i := 0; i < len(scanners); i++ {
-			for j := i + 1; j < len(scanners); j++ {
-				if ok, transform, correction := Overlap(scanners[i], scanners[j]); ok {
-					// We've found two scanners that overlap.  Merge the beacons from scanner j into
-					// scanner i, being sure to translate them to scanner i's coordinates first.
-					for _, e := range scanners[j].Beacons.Entries() {
-						b := e.(Point3D)
-						scanners[i].Beacons.Add(correction(transform(b)))
-					}
-
-					// Remove scanner j
-					scanners[j] = scanners[len(scanners)-1]
-					scanners = scanners[:len(scanners)-1]
-					continue outer
-				}
+	// Determine which scanners overlap.  The overlapping scanners will form a graph
+	// that can be used to determine the path to take to convert coordinates of the
+	// beacons from any scanner into the coordinate system of any other scanner.
+	edges := NewMatrix(len(scanners))
+	for i := 0; i < len(scanners); i++ {
+		for j := i + 1; j < len(scanners); j++ {
+			if ok, iBeacon, jBeacon := Overlaps(scanners[i], scanners[j]); ok {
+				edges[i][j] = BuildTransform(scanners[i].Beacons, scanners[j].Beacons, iBeacon, jBeacon)
+				edges[j][i] = BuildTransform(scanners[j].Beacons, scanners[i].Beacons, jBeacon, iBeacon)
 			}
 		}
 	}
 
-	fmt.Println("# beacons:", scanners[0].Beacons.Size())
+	// Now that we have the graph, determine the path we have to take to convert each
+	// scanner's beacons into the coordinate system of the first scanner and use it to
+	// build a transform.
+	transforms := make([]Transform, len(scanners))
+	transforms[0] = func(p Point3D) Point3D { return p }
+
+	isGoal := func(node aoc.Node) bool {
+		return node.(*ScannerNode).id == 0
+	}
+	for i := 1; i < len(scanners); i++ {
+		path, ok := aoc.BreadthFirstSearch(&ScannerNode{id: i, edges: edges}, isGoal)
+		if !ok {
+			log.Fatalf("unable to determine path to scanner 0 for scanner %d", i)
+		}
+
+		var funcs []Transform
+		for j := 0; j < len(path)-1; j++ {
+			current := path[j].(*ScannerNode).id
+			next := path[j+1].(*ScannerNode).id
+			funcs = append(funcs, edges[current][next])
+		}
+
+		transforms[i] = func(p Point3D) Point3D {
+			for i := 0; i < len(funcs); i++ {
+				p = funcs[i](p)
+			}
+			return p
+		}
+	}
+
+	// Finally, use each transform to convert every beacon into the coordinate
+	// space of scanner 0 so that we can determine how many beacons there actually
+	// are.
+	beacons := aoc.NewSet()
+	for i, scanner := range scanners {
+		for _, b := range scanner.Beacons {
+			beacons.Add(transforms[i](b))
+		}
+	}
+	fmt.Println(beacons.Size())
 }
 
-func Overlap(a, b Scanner) (bool, Transform, Transform) {
+func NewMatrix(n int) [][]Transform {
+	var m [][]Transform
+	for i := 0; i < n; i++ {
+		m = append(m, make([]Transform, n))
+	}
+	return m
+}
+
+func Overlaps(a, b *Scanner) (bool, Point3D, Point3D) {
+	// Find a pair of beacons across the scanners that share distances to other beacons.
+	// If there are 12 or more distances that are the same then the beacons are considered
+	// to be the same.
+	for i := 0; i < len(a.Beacons); i++ {
+		for j := 0; j < len(b.Beacons); j++ {
+			if a.Distances[i].Intersect(b.Distances[j]).Size() >= 12 {
+				return true, a.Beacons[i], b.Beacons[j]
+			}
+		}
+	}
+	return false, Point3D{}, Point3D{}
+}
+
+// BuildTransform takes two sets of beacons that are known to overlap and determines
+// transform functions that will convert coordinates from a to b.
+func BuildTransform(a, b []Point3D, aBeacon, bBeacon Point3D) Transform {
+	// We don't yet know which transform should be used, so try them all and see which
+	// one works.
 	for _, transform := range Transforms {
-		bBeacons := b.GetTransformedBeacons(transform)
+		dx := bBeacon.X - transform(aBeacon).X
+		dy := bBeacon.Y - transform(aBeacon).Y
+		dz := bBeacon.Z - transform(aBeacon).Z
+		convert := func(p Point3D) Point3D {
+			p = transform(p)
+			p.X += dx
+			p.Y += dy
+			p.Z += dz
+			return p
+		}
 
-		// Give every pair of beacons (one from A and one from B) the chance to be the same
-		// beacon in different coordinate systems.  This will cause us to generate a
-		// correction factor for B's beacons.  If the beacons are truly the same, then at
-		// least 12 other beacons in B should become beacons of A.
-		for _, aEntry := range a.Beacons.Entries() {
-			aBeacon := aEntry.(Point3D)
+		// Transform and translate a beacons
+		aSet := aoc.NewSet()
+		for _, p := range a {
+			aSet.Add(convert(p))
+		}
 
-			for _, bEntry := range bBeacons.Entries() {
-				bBeacon := bEntry.(Point3D)
-				dx := bBeacon.X - aBeacon.X
-				dy := bBeacon.Y - aBeacon.Y
-				dz := bBeacon.Z - aBeacon.Z
-				correction := func(p Point3D) Point3D {
-					return Point3D{p.X - dx, p.Y - dy, p.Z - dz}
-				}
+		// Now count how many b's match.
+		var count int
+		for _, bBeacon := range b {
+			if aSet.Contains(bBeacon) {
+				count++
+			}
+		}
 
-				matches := CountMatches(a.Beacons, bBeacons, correction)
-				if matches >= 12 {
-					// fmt.Println("  transform", transform(Point3D{1, 2, 3}))
-					// fmt.Println("  correction", correction(Point3D{}))
-					return true, transform, correction
-				}
+		if count >= 12 {
+			// We have a match, return a function that converts from a's to b's.
+			return func(p Point3D) Point3D {
+				p = transform(p)
+				p.X += dx
+				p.Y += dy
+				p.Z += dz
+				return p
 			}
 		}
 	}
-	return false, nil, nil
-}
 
-func CountMatches(as, bs aoc.Set, transform Transform) int {
-	bSet := aoc.NewSet()
-	for _, b := range bs.Entries() {
-		bSet.Add(transform(b.(Point3D)))
-	}
-
-	return as.Intersect(bSet).Size()
-}
-
-type Point3D struct {
-	X, Y, Z int
-}
-
-type Scanner struct {
-	ID      string
-	Beacons aoc.Set
+	return nil
 }
 
 type Transform func(Point3D) Point3D
@@ -115,37 +162,89 @@ var Transforms = []Transform{
 	func(p Point3D) Point3D { return Point3D{-p.Z, p.X, -p.Y} },
 }
 
-func (s Scanner) GetTransformedBeacons(t Transform) aoc.Set {
-	bs := aoc.NewSet()
-	for _, b := range s.Beacons.Entries() {
-		bs.Add(t(b.(Point3D)))
-	}
-	return bs
+type Scanner struct {
+	Beacons   []Point3D
+	Distances []aoc.Set
 }
 
-func InputToScanners() []Scanner {
-	var scanners []Scanner
+func (s *Scanner) AddBeacon(p Point3D) {
+	index := len(s.Beacons)
 
-	var scanner = Scanner{Beacons: aoc.NewSet()}
+	s.Beacons = append(s.Beacons, p)
+	s.Distances = append(s.Distances, aoc.NewSet())
+
+	for i, b := range s.Beacons {
+		distance := p.Distance2(b)
+		s.Distances[i].Add(distance)
+		s.Distances[index].Add(distance)
+	}
+}
+
+type ScannerNode struct {
+	id    int
+	edges [][]Transform
+}
+
+func (sn *ScannerNode) ID() string {
+	return fmt.Sprintf("%d", sn.id)
+}
+
+func (sn *ScannerNode) Children() []aoc.Node {
+	var children []aoc.Node
+	for i := 0; i < len(sn.edges); i++ {
+		if sn.edges[sn.id][i] != nil {
+			children = append(children, &ScannerNode{id: i, edges: sn.edges})
+		}
+	}
+	return children
+}
+
+func InputToScanners() []*Scanner {
+	var scanners []*Scanner
+
+	var scanner = new(Scanner)
 	for _, line := range aoc.InputToLines(2021, 19) {
 		var id string
 		if _, err := fmt.Sscanf(line, "--- scanner %s ---", &id); err == nil {
-			scanner.ID = id
 			scanners = append(scanners, scanner)
 			continue
 		}
 
 		if line == "" {
-			scanner = Scanner{Beacons: aoc.NewSet()}
+			scanner = new(Scanner)
 			continue
 		}
 
 		var x, y, z int
 		if _, err := fmt.Sscanf(line, "%d,%d,%d", &x, &y, &z); err == nil {
-			scanner.Beacons.Add(Point3D{X: x, Y: y, Z: z})
+			scanner.AddBeacon(Point3D{X: x, Y: y, Z: z})
 			continue
 		}
 	}
 
+	// Now that the scanners have been created determine the distance from each beacon to
+	// each other beacon.
+	for _, s := range scanners {
+		for _, origin := range s.Beacons {
+			distances := aoc.NewSet()
+			for _, destination := range s.Beacons {
+				distances.Add(origin.Distance2(destination))
+			}
+
+			s.Distances = append(s.Distances, distances)
+		}
+	}
+
 	return scanners
+}
+
+type Point3D struct {
+	X, Y, Z int
+}
+
+func (p Point3D) Distance2(other Point3D) int {
+	dx := other.X - p.X
+	dy := other.Y - p.Y
+	dz := other.Z - p.Z
+	return dx*dx + dy*dy + dz*dz
 }
--- cmd/2021/19-2/main.go
+++ cmd/2021/19-2/main.go
@@ -2,114 +2,139 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/bbeck/advent-of-code/aoc"
+	"log"
 )
 
 func main() {
 	scanners := InputToScanners()
-	fmt.Println("num scanners:", len(scanners))
 
-outer:
-	for len(scanners) > 1 {
-		fmt.Println("there are", len(scanners), "scanners remaining")
-		for i := 0; i < len(scanners); i++ {
-			for j := i + 1; j < len(scanners); j++ {
-				if ok, transform, correction := Overlap(scanners[i], scanners[j]); ok {
-					// We've found two scanners that overlap.  Merge the beacons from scanner j into
-					// scanner i, being sure to translate them to scanner i's coordinates first.
-					for _, e := range scanners[j].Beacons.Entries() {
-						b := e.(Point3D)
-						scanners[i].Beacons.Add(correction(transform(b)))
-					}
-
-					for _, l := range scanners[j].ScannerLocations {
-						scanners[i].ScannerLocations = append(scanners[i].ScannerLocations, correction(transform(l)))
-					}
-
-					// Remove scanner j
-					scanners[j] = scanners[len(scanners)-1]
-					scanners = scanners[:len(scanners)-1]
-					continue outer
-				}
+	// Determine which scanners overlap.  The overlapping scanners will form a graph
+	// that can be used to determine the path to take to convert coordinates of the
+	// beacons from any scanner into the coordinate system of any other scanner.
+	edges := NewMatrix(len(scanners))
+	for i := 0; i < len(scanners); i++ {
+		for j := i + 1; j < len(scanners); j++ {
+			if ok, iBeacon, jBeacon := Overlaps(scanners[i], scanners[j]); ok {
+				edges[i][j] = BuildTransform(scanners[i].Beacons, scanners[j].Beacons, iBeacon, jBeacon)
+				edges[j][i] = BuildTransform(scanners[j].Beacons, scanners[i].Beacons, jBeacon, iBeacon)
 			}
 		}
 	}
 
-	fmt.Println("# beacons:", scanners[0].Beacons.Size())
+	// Now that we have the graph, determine the path we have to take to convert each
+	// scanner's beacons into the coordinate system of the first scanner and use it to
+	// build a transform.
+	transforms := make([]Transform, len(scanners))
+	transforms[0] = func(p Point3D) Point3D { return p }
 
-	locations := scanners[0].ScannerLocations
-	locations = append(locations, Point3D{})
+	isGoal := func(node aoc.Node) bool {
+		return node.(*ScannerNode).id == 0
+	}
+	for i := 1; i < len(scanners); i++ {
+		path, ok := aoc.BreadthFirstSearch(&ScannerNode{id: i, edges: edges}, isGoal)
+		if !ok {
+			log.Fatalf("unable to determine path to scanner 0 for scanner %d", i)
+		}
 
-	fmt.Println("locations:")
-	for _, l := range scanners[0].ScannerLocations {
-		fmt.Println("  ", l)
+		var funcs []Transform
+		for j := 0; j < len(path)-1; j++ {
+			current := path[j].(*ScannerNode).id
+			next := path[j+1].(*ScannerNode).id
+			funcs = append(funcs, edges[current][next])
+		}
+
+		transforms[i] = func(p Point3D) Point3D {
+			for i := 0; i < len(funcs); i++ {
+				p = funcs[i](p)
+			}
+			return p
+		}
 	}
 
+	// Finally, use each transform to determine the position of each scanner in
+	// scanner 0's coordinate system.
+	var coordinates []Point3D
+	for i := range scanners {
+		coordinates = append(coordinates, transforms[i](Point3D{}))
+	}
+
+	// Determine the pair with the largest manhattan distance.
 	var max int
-	for i := 0; i < len(locations); i++ {
-		for j := i + 1; j < len(locations); j++ {
-			max = aoc.MaxInt(max, locations[i].ManhattanDistance(locations[j]))
+	for i := 0; i < len(scanners); i++ {
+		for j := i + 1; j < len(scanners); j++ {
+			max = aoc.MaxInt(max, coordinates[i].ManhattanDistance(coordinates[j]))
 		}
 	}
 	fmt.Println(max)
 }
 
-func Overlap(a, b Scanner) (bool, Transform, Transform) {
-	for _, transform := range Transforms {
-		bBeacons := b.GetTransformedBeacons(transform)
+func NewMatrix(n int) [][]Transform {
+	var m [][]Transform
+	for i := 0; i < n; i++ {
+		m = append(m, make([]Transform, n))
+	}
+	return m
+}
 
-		// Give every pair of beacons (one from A and one from B) the chance to be the same
-		// beacon in different coordinate systems.  This will cause us to generate a
-		// correction factor for B's beacons.  If the beacons are truly the same, then at
-		// least 12 other beacons in B should become beacons of A.
-		for _, aEntry := range a.Beacons.Entries() {
-			aBeacon := aEntry.(Point3D)
-
-			for _, bEntry := range bBeacons.Entries() {
-				bBeacon := bEntry.(Point3D)
-				dx := bBeacon.X - aBeacon.X
-				dy := bBeacon.Y - aBeacon.Y
-				dz := bBeacon.Z - aBeacon.Z
-				correction := func(p Point3D) Point3D {
-					return Point3D{p.X - dx, p.Y - dy, p.Z - dz}
-				}
-
-				matches := CountMatches(a.Beacons, bBeacons, correction)
-				if matches >= 12 {
-					return true, transform, correction
-				}
+func Overlaps(a, b *Scanner) (bool, Point3D, Point3D) {
+	// Find a pair of beacons across the scanners that share distances to other beacons.
+	// If there are 12 or more distances that are the same then the beacons are considered
+	// to be the same.
+	for i := 0; i < len(a.Beacons); i++ {
+		for j := 0; j < len(b.Beacons); j++ {
+			if a.Distances[i].Intersect(b.Distances[j]).Size() >= 12 {
+				return true, a.Beacons[i], b.Beacons[j]
 			}
 		}
 	}
-	return false, nil, nil
+	return false, Point3D{}, Point3D{}
 }
 
-func CountMatches(as, bs aoc.Set, transform Transform) int {
-	bSet := aoc.NewSet()
-	for _, b := range bs.Entries() {
-		bSet.Add(transform(b.(Point3D)))
+// BuildTransform takes two sets of beacons that are known to overlap and determines
+// transform functions that will convert coordinates from a to b.
+func BuildTransform(a, b []Point3D, aBeacon, bBeacon Point3D) Transform {
+	// We don't yet know which transform should be used, so try them all and see which
+	// one works.
+	for _, transform := range Transforms {
+		dx := bBeacon.X - transform(aBeacon).X
+		dy := bBeacon.Y - transform(aBeacon).Y
+		dz := bBeacon.Z - transform(aBeacon).Z
+		convert := func(p Point3D) Point3D {
+			p = transform(p)
+			p.X += dx
+			p.Y += dy
+			p.Z += dz
+			return p
+		}
+
+		// Transform and translate a beacons
+		aSet := aoc.NewSet()
+		for _, p := range a {
+			aSet.Add(convert(p))
+		}
+
+		// Now count how many b's match.
+		var count int
+		for _, bBeacon := range b {
+			if aSet.Contains(bBeacon) {
+				count++
+			}
+		}
+
+		if count >= 12 {
+			// We have a match, return a function that converts from a's to b's.
+			return func(p Point3D) Point3D {
+				p = transform(p)
+				p.X += dx
+				p.Y += dy
+				p.Z += dz
+				return p
+			}
+		}
 	}
 
-	return as.Intersect(bSet).Size()
-}
-
-type Point3D struct {
-	X, Y, Z int
-}
-
-func (p Point3D) ManhattanDistance(q Point3D) int {
-	dx := aoc.AbsInt(p.X - q.X)
-	dy := aoc.AbsInt(p.Y - q.Y)
-	dz := aoc.AbsInt(p.Z - q.Z)
-
-	return dx + dy + dz
-}
-
-type Scanner struct {
-	ID               string
-	Beacons          aoc.Set
-	ScannerLocations []Point3D
+	return nil
 }
 
 type Transform func(Point3D) Point3D
@@ -142,37 +167,96 @@ var Transforms = []Transform{
 	func(p Point3D) Point3D { return Point3D{-p.Z, p.X, -p.Y} },
 }
 
-func (s Scanner) GetTransformedBeacons(t Transform) aoc.Set {
-	bs := aoc.NewSet()
-	for _, b := range s.Beacons.Entries() {
-		bs.Add(t(b.(Point3D)))
-	}
-	return bs
+type Scanner struct {
+	Beacons   []Point3D
+	Distances []aoc.Set
 }
 
-func InputToScanners() []Scanner {
-	var scanners []Scanner
+func (s *Scanner) AddBeacon(p Point3D) {
+	index := len(s.Beacons)
 
-	var scanner = Scanner{Beacons: aoc.NewSet()}
+	s.Beacons = append(s.Beacons, p)
+	s.Distances = append(s.Distances, aoc.NewSet())
+
+	for i, b := range s.Beacons {
+		distance := p.Distance2(b)
+		s.Distances[i].Add(distance)
+		s.Distances[index].Add(distance)
+	}
+}
+
+type ScannerNode struct {
+	id    int
+	edges [][]Transform
+}
+
+func (sn *ScannerNode) ID() string {
+	return fmt.Sprintf("%d", sn.id)
+}
+
+func (sn *ScannerNode) Children() []aoc.Node {
+	var children []aoc.Node
+	for i := 0; i < len(sn.edges); i++ {
+		if sn.edges[sn.id][i] != nil {
+			children = append(children, &ScannerNode{id: i, edges: sn.edges})
+		}
+	}
+	return children
+}
+
+func InputToScanners() []*Scanner {
+	var scanners []*Scanner
+
+	var scanner = new(Scanner)
 	for _, line := range aoc.InputToLines(2021, 19) {
 		var id string
 		if _, err := fmt.Sscanf(line, "--- scanner %s ---", &id); err == nil {
-			scanner.ID = id
 			scanners = append(scanners, scanner)
 			continue
 		}
 
 		if line == "" {
-			scanner = Scanner{Beacons: aoc.NewSet(), ScannerLocations: []Point3D{{}}}
+			scanner = new(Scanner)
 			continue
 		}
 
 		var x, y, z int
 		if _, err := fmt.Sscanf(line, "%d,%d,%d", &x, &y, &z); err == nil {
-			scanner.Beacons.Add(Point3D{X: x, Y: y, Z: z})
+			scanner.AddBeacon(Point3D{X: x, Y: y, Z: z})
 			continue
 		}
 	}
 
+	// Now that the scanners have been created determine the distance from each beacon to
+	// each other beacon.
+	for _, s := range scanners {
+		for _, origin := range s.Beacons {
+			distances := aoc.NewSet()
+			for _, destination := range s.Beacons {
+				distances.Add(origin.Distance2(destination))
+			}
+
+			s.Distances = append(s.Distances, distances)
+		}
+	}
+
 	return scanners
+}
+
+type Point3D struct {
+	X, Y, Z int
+}
+
+func (p Point3D) Distance2(other Point3D) int {
+	dx := other.X - p.X
+	dy := other.Y - p.Y
+	dz := other.Z - p.Z
+	return dx*dx + dy*dy + dz*dz
+}
+
+func (p Point3D) ManhattanDistance(other Point3D) int {
+	dx := aoc.AbsInt(other.X - p.X)
+	dy := aoc.AbsInt(other.Y - p.Y)
+	dz := aoc.AbsInt(other.Z - p.Z)
+	return dx + dy + dz
 }
