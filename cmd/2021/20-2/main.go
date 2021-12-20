bf5ac758e5b1e6f8715d09ae598b49c5e251cf2d
Brandon Beck <bmbeck@gmail.com>
2021-12-20 10:14:49 -0600

    Refactor 2021/20

--- cmd/2021/20-2/main.go
+++ cmd/2021/20-2/main.go
@@ -8,8 +8,12 @@ import (
 func main() {
 	algorithm, image := InputToAlgorithmAndImage()
 
+	// The border is infinite, so depending on the value of the algorithm when you have
+	// a solid block of '.' or '#' it might toggle for each step.
+	toggles := algorithm[0] == '#' && algorithm[511] == '.'
+
 	for step := 0; step < 50; step++ {
-		image = Enhance(image, algorithm, algorithm[0] == '#' && step%2 == 1)
+		image = Enhance(image, algorithm, toggles && step%2 == 1)
 	}
 
 	var count int
@@ -21,47 +25,22 @@ func main() {
 	fmt.Println(count)
 }
 
-func Show(image map[aoc.Point2D]bool, border bool) {
+func GetBounds(image map[aoc.Point2D]bool) (int, int, int, int) {
 	var points []aoc.Point2D
 	for p := range image {
 		points = append(points, p)
 	}
-
-	minX, minY, maxX, maxY := aoc.GetBounds(points)
-	for y := minY; y <= maxY; y++ {
-		for x := minX; x <= maxX; x++ {
-			q := aoc.Point2D{X: x, Y: y}
-
-			var value bool
-			if q.X < minX || q.Y < minY || q.X > maxX || q.Y > maxY {
-				value = border
-			} else {
-				value = image[q]
-			}
-
-			if value {
-				fmt.Print("#")
-			} else {
-				fmt.Print(".")
-			}
-		}
-		fmt.Println()
-	}
-	fmt.Println()
+	return aoc.GetBounds(points)
 }
 
 func Enhance(image map[aoc.Point2D]bool, algorithm string, border bool) map[aoc.Point2D]bool {
-	var points []aoc.Point2D
-	for p := range image {
-		points = append(points, p)
-	}
-	minX, minY, maxX, maxY := aoc.GetBounds(points)
+	minX, minY, maxX, maxY := GetBounds(image)
 
 	output := make(map[aoc.Point2D]bool)
 	for x := minX - 2; x <= maxX+2; x++ {
 		for y := minY - 2; y <= maxY+2; y++ {
 			p := aoc.Point2D{X: x, Y: y}
-			index := GetNumber(p, image, border)
+			index := GetIndex(p, image, border)
 
 			output[p] = algorithm[index] == '#'
 		}
@@ -70,7 +49,7 @@ func Enhance(image map[aoc.Point2D]bool, algorithm string, border bool) map[aoc.
 	return output
 }
 
-func GetNumber(p aoc.Point2D, image map[aoc.Point2D]bool, border bool) int {
+func GetIndex(p aoc.Point2D, image map[aoc.Point2D]bool, border bool) int {
 	var n int
 	for dy := -1; dy <= 1; dy++ {
 		for dx := -1; dx <= 1; dx++ {
