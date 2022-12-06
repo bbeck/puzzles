c1298a712bc665d6cbaf4cd2b2cef163e75d8ba9
Brandon Beck <bmbeck@gmail.com>
2022-12-05 23:07:48 -0600

    2022/06

---
+++ cmd/2022/06-2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bbeck/advent-of-code/aoc"
+)
+
+const N = 14
+
+func main() {
+	s := aoc.InputToString(2022, 6)
+	for end := N; end < len(s); end++ {
+		bs := []byte(s[end-N : end])
+		if len(aoc.SetFrom(bs...)) == N {
+			fmt.Println(end)
+			break
+		}
+	}
+}
