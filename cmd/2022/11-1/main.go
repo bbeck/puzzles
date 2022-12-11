dc1caeec5e39bf05942c099ac57cc56b176c6742
Brandon Beck <bmbeck@gmail.com>
2022-12-11 01:16:26 -0600

    2022/11

---
+++ cmd/2022/11-1/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bbeck/advent-of-code/aoc"
+	"sort"
+	"strings"
+)
+
+func main() {
+	monkeys := InputToMonkeys()
+
+	for n := 0; n < 20; n++ {
+		for _, monkey := range monkeys {
+			for !monkey.Items.Empty() {
+				monkey.Inspections++
+
+				worry := monkey.Op(monkey.Items.PopFront()) / 3
+				if worry%monkey.Mod == 0 {
+					monkeys[monkey.TrueID].Items.PushBack(worry)
+				} else {
+					monkeys[monkey.FalseID].Items.PushBack(worry)
+				}
+			}
+		}
+	}
+
+	sort.Slice(monkeys, func(i, j int) bool {
+		return monkeys[i].Inspections > monkeys[j].Inspections
+	})
+	fmt.Println(monkeys[0].Inspections * monkeys[1].Inspections)
+}
+
+type Monkey struct {
+	Items       *aoc.Deque[int]
+	Op          func(int) int
+	Mod         int
+	TrueID      int
+	FalseID     int
+	Inspections int
+}
+
+func InputToMonkeys() []*Monkey {
+	lines := aoc.InputToLines(2022, 11)
+
+	var monkeys []*Monkey
+	for i := 0; i < len(lines); i += 7 {
+		var id int
+		fmt.Sscanf(lines[i+0], "Monkey %d:", &id)
+
+		var monkey Monkey
+		monkey.Items = new(aoc.Deque[int])
+		for _, s := range strings.Split(lines[i+1][18:], ", ") {
+			monkey.Items.PushBack(aoc.ParseInt(s))
+		}
+
+		monkey.Op = ParseOp(lines[i+2][19:])
+		monkey.Mod = aoc.ParseInt(lines[i+3][21:])
+		monkey.TrueID = aoc.ParseInt(lines[i+4][29:])
+		monkey.FalseID = aoc.ParseInt(lines[i+5][30:])
+
+		monkeys = append(monkeys, &monkey)
+	}
+
+	return monkeys
+}
+
+func ParseOp(s string) func(int) int {
+	fields := strings.Fields(s)
+	_, op, rhs := fields[0], fields[1], fields[2]
+
+	switch {
+	case op == "+" && rhs != "old":
+		n := aoc.ParseInt(rhs)
+		return func(old int) int { return old + n }
+
+	case op == "+" && rhs == "old":
+		return func(old int) int { return old + old }
+
+	case op == "*" && rhs != "old":
+		n := aoc.ParseInt(rhs)
+		return func(old int) int { return old * n }
+
+	case op == "*" && rhs == "old":
+		return func(old int) int { return old * old }
+	}
+
+	return nil
+}
