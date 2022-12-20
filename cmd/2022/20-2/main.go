f55900dfb106e16068c1081c0fd066dc35fc6ac9
Brandon Beck <bmbeck@gmail.com>
2022-12-20 10:12:06 -0600

    2022/20

---
+++ cmd/2022/20-2/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bbeck/advent-of-code/aoc"
+)
+
+const Key = 811589153
+
+func main() {
+	nodes, zero := InputToNodes()
+	N := len(nodes)
+
+	for iter := 0; iter < 10; iter++ {
+		for _, n := range nodes {
+			// First remove it from the list.
+			n.Prev.Next = n.Next
+			n.Next.Prev = n.Prev
+
+			// Advance until we're at the insertion location.
+			steps := n.Value % (N - 1)
+			if steps < 0 {
+				steps += N - 1
+			}
+			for steps > 0 {
+				n.Next = n.Next.Next
+				steps--
+			}
+
+			// Insert it back into its new location.
+			n.Prev = n.Next.Prev
+			n.Prev.Next = n
+			n.Next.Prev = n
+		}
+	}
+
+	var sum int
+	for i := 0; i < 3; i++ {
+		steps := 1000 % N
+		for steps > 0 {
+			zero = zero.Next
+			steps--
+		}
+
+		sum += zero.Value
+	}
+	fmt.Println(sum)
+}
+
+type Node struct {
+	Value      int
+	Prev, Next *Node
+}
+
+func InputToNodes() ([]*Node, *Node) {
+	var nodes []*Node
+	var zero *Node
+	for _, n := range aoc.InputToInts(2022, 20) {
+		node := &Node{Value: n * Key}
+		nodes = append(nodes, node)
+
+		if n == 0 {
+			zero = node
+		}
+	}
+
+	N := len(nodes)
+	for i, n := range nodes {
+		n.Prev = nodes[(i-1+N)%N]
+		n.Next = nodes[(i+1+N)%N]
+	}
+
+	return nodes, zero
+}
