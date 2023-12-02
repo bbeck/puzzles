b117b2916a2b7caec2e17c6659678aef020f3c01
Brandon Beck <bmbeck@gmail.com>
2023-12-02 01:02:41 -0600

    2023/02

---
+++ cmd/2023/02-1/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bbeck/advent-of-code/aoc"
+	"strings"
+)
+
+func main() {
+	var sum int
+	for _, game := range InputToGames() {
+		if game.IsOk() {
+			sum += game.ID
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+type Game struct {
+	ID      int
+	Subsets []map[string]int
+}
+
+func (g *Game) IsOk() bool {
+	for _, s := range g.Subsets {
+		for color, num := range s {
+			switch {
+			case color == "red" && num > 12:
+				return false
+			case color == "green" && num > 13:
+				return false
+			case color == "blue" && num > 14:
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func InputToGames() []Game {
+	return aoc.InputLinesTo(2023, 2, func(line string) (Game, error) {
+		line = strings.ReplaceAll(line, "Game ", "")
+		line = strings.ReplaceAll(line, ":", "")
+		line = strings.ReplaceAll(line, ",", "")
+		line = strings.ReplaceAll(line, "; ", " ; ")
+		fields := strings.Fields(line)
+
+		subsets := []map[string]int{{}}
+		for i := 1; i < len(fields); i++ {
+			if fields[i] == ";" {
+				subsets = append(subsets, make(map[string]int))
+				continue
+			}
+
+			subsets[len(subsets)-1][fields[i+1]] = aoc.ParseInt(fields[i])
+			i++
+		}
+
+		return Game{
+			ID:      aoc.ParseInt(fields[0]),
+			Subsets: subsets,
+		}, nil
+	})
+}
---
+++ cmd/2023/02-2/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bbeck/advent-of-code/aoc"
+	"strings"
+)
+
+func main() {
+	var sum int
+	for _, game := range InputToGames() {
+		sum += game.Power()
+	}
+
+	fmt.Println(sum)
+}
+
+type Game struct {
+	ID      int
+	Subsets []map[string]int
+}
+
+func (g *Game) Power() int {
+	var maxRed, maxBlue, maxGreen int
+	for _, s := range g.Subsets {
+		maxRed = aoc.Max(maxRed, s["red"])
+		maxGreen = aoc.Max(maxGreen, s["green"])
+		maxBlue = aoc.Max(maxBlue, s["blue"])
+	}
+
+	return maxRed * maxBlue * maxGreen
+}
+
+func InputToGames() []Game {
+	return aoc.InputLinesTo(2023, 2, func(line string) (Game, error) {
+		line = strings.ReplaceAll(line, "Game ", "")
+		line = strings.ReplaceAll(line, ":", "")
+		line = strings.ReplaceAll(line, ",", "")
+		line = strings.ReplaceAll(line, "; ", " ; ")
+		fields := strings.Fields(line)
+
+		subsets := []map[string]int{{}}
+		for i := 1; i < len(fields); i++ {
+			if fields[i] == ";" {
+				subsets = append(subsets, make(map[string]int))
+				continue
+			}
+
+			subsets[len(subsets)-1][fields[i+1]] = aoc.ParseInt(fields[i])
+			i++
+		}
+
+		return Game{
+			ID:      aoc.ParseInt(fields[0]),
+			Subsets: subsets,
+		}, nil
+	})
+}
