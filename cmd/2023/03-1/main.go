5d520288680a3a18483214645096187e91b46997
Brandon Beck <bmbeck@gmail.com>
2023-12-03 21:53:14 -0600

    2023/03

---
+++ cmd/2023/03-1/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bbeck/advent-of-code/aoc"
+	"strings"
+)
+
+func main() {
+	grid := aoc.InputToGrid2D(2023, 3, func(x, y int, s string) string {
+		return s
+	})
+
+	var sum int
+	ForEachNumber(grid, func(x int, y int, num int) {
+		for n := num; n > 0; n = n / 10 {
+			grid.ForEachNeighbor(x, y, func(_ int, _ int, s string) {
+				if IsSymbol(s) {
+					sum += num
+					n = 0
+				}
+			})
+
+			x++
+		}
+	})
+
+	fmt.Println(sum)
+}
+
+func ForEachNumber(g aoc.Grid2D[string], fn func(int, int, int)) {
+	g.ForEach(func(x0 int, y int, s string) {
+		// Check if this is the beginning of a number
+		if IsDigit(s) && (x0 == 0 || !IsDigit(g.Get(x0-1, y))) {
+			var digits strings.Builder
+			for x := x0; x < g.Width && IsDigit(g.Get(x, y)); x++ {
+				digits.WriteString(g.Get(x, y))
+			}
+
+			fn(x0, y, aoc.ParseInt(digits.String()))
+		}
+	})
+}
+
+func IsDigit(s string) bool {
+	return strings.Contains("0123456789", s)
+}
+
+func IsSymbol(s string) bool {
+	return s != "." && !IsDigit(s)
+}
---
+++ cmd/2023/03-2/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bbeck/advent-of-code/aoc"
+	"strings"
+)
+
+func main() {
+	grid := aoc.InputToGrid2D(2023, 3, func(x, y int, s string) string {
+		return s
+	})
+
+	gears := make(map[aoc.Point2D][]int)
+	ForEachNumber(grid, func(x int, y int, n int) {
+		digits := aoc.Digits(n)
+
+		for dy := -1; dy <= 1; dy++ {
+			for dx := -1; dx <= len(digits); dx++ {
+				p := aoc.Point2D{X: x + dx, Y: y + dy}
+
+				if grid.InBoundsPoint(p) && grid.GetPoint(p) == "*" {
+					gears[p] = append(gears[p], n)
+				}
+			}
+		}
+	})
+
+	var prod int
+	for _, nums := range gears {
+		if len(nums) == 2 {
+			prod += aoc.Product(nums...)
+		}
+	}
+	fmt.Println(prod)
+}
+
+func ForEachNumber(g aoc.Grid2D[string], fn func(int, int, int)) {
+	g.ForEach(func(x0 int, y int, s string) {
+		// Check if this is the beginning of a number
+		if IsDigit(s) && (x0 == 0 || !IsDigit(g.Get(x0-1, y))) {
+			var digits strings.Builder
+			for x := x0; x < g.Width && IsDigit(g.Get(x, y)); x++ {
+				digits.WriteString(g.Get(x, y))
+			}
+
+			fn(x0, y, aoc.ParseInt(digits.String()))
+		}
+	})
+}
+
+func IsDigit(s string) bool {
+	return strings.Contains("0123456789", s)
+}
