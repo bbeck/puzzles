75f7f768220bfedf57ebff706d59e103b47f6795
Brandon Beck <bmbeck@gmail.com>
2023-12-04 22:42:18 -0600

    Optimize 2023/04

--- cmd/2023/04-1/main.go
+++ cmd/2023/04-1/main.go
@@ -8,42 +8,29 @@ import (
 
 func main() {
 	var points int
-	for _, card := range InputToCards() {
-		common := len(card.Winning.Intersect(card.Numbers))
-		if common == 0 {
-			continue
+	for _, winning := range InputToNumWinningNumbers() {
+		if winning != 0 {
+			points += aoc.Pow(2, uint(winning-1))
 		}
-
-		points += aoc.Pow(2, uint(common-1))
 	}
 
 	fmt.Println(points)
 }
 
-type Card struct {
-	ID      int
-	Winning aoc.Set[int]
-	Numbers aoc.Set[int]
-}
+func InputToNumWinningNumbers() []int {
+	return aoc.InputLinesTo(2023, 4, func(line string) (int, error) {
+		// A number is only winning if it appears more than once per line.
+		line = strings.ReplaceAll(line, "|", "")
+		_, rhs, _ := strings.Cut(line, ":")
 
-func InputToCards() []Card {
-	return aoc.InputLinesTo(2023, 4, func(line string) (Card, error) {
-		line = strings.ReplaceAll(line, "Card ", "")
-		line = strings.ReplaceAll(line, ":", "")
-
-		fields := aoc.DequeFrom(strings.Fields(line)...)
-		id := aoc.ParseInt(fields.PopFront())
-
-		var winning aoc.Set[int]
-		for field := fields.PopFront(); field != "|"; field = fields.PopFront() {
-			winning.Add(aoc.ParseInt(field))
+		var seen, winning aoc.Set[int]
+		for _, field := range strings.Fields(rhs) {
+			num := aoc.ParseInt(field)
+			if !seen.Add(num) {
+				winning.Add(num)
+			}
 		}
 
-		var numbers aoc.Set[int]
-		for !fields.Empty() {
-			numbers.Add(aoc.ParseInt(fields.PopFront()))
-		}
-
-		return Card{ID: id, Winning: winning, Numbers: numbers}, nil
+		return len(winning), nil
 	})
 }
--- cmd/2023/04-2/main.go
+++ cmd/2023/04-2/main.go
@@ -8,12 +8,11 @@ import (
 
 func main() {
 	counts := make(map[int]int)
-	for _, card := range InputToCards() {
-		counts[card.ID]++
+	for id, winning := range InputToNumWinningNumbers() {
+		counts[id]++
 
-		common := len(card.Winning.Intersect(card.Numbers))
-		for i := 1; i <= common; i++ {
-			counts[card.ID+i] += counts[card.ID]
+		for i := 1; i <= winning; i++ {
+			counts[id+i] += counts[id]
 		}
 	}
 
@@ -24,30 +23,20 @@ func main() {
 	fmt.Println(total)
 }
 
-type Card struct {
-	ID      int
-	Winning aoc.Set[int]
-	Numbers aoc.Set[int]
-}
+func InputToNumWinningNumbers() []int {
+	return aoc.InputLinesTo(2023, 4, func(line string) (int, error) {
+		// A number is only winning if it appears more than once per line.
+		line = strings.ReplaceAll(line, "|", "")
+		_, rhs, _ := strings.Cut(line, ":")
 
-func InputToCards() []Card {
-	return aoc.InputLinesTo(2023, 4, func(line string) (Card, error) {
-		line = strings.ReplaceAll(line, "Card ", "")
-		line = strings.ReplaceAll(line, ":", "")
-
-		fields := aoc.DequeFrom(strings.Fields(line)...)
-		id := aoc.ParseInt(fields.PopFront())
-
-		var winning aoc.Set[int]
-		for field := fields.PopFront(); field != "|"; field = fields.PopFront() {
-			winning.Add(aoc.ParseInt(field))
+		var seen, winning aoc.Set[int]
+		for _, field := range strings.Fields(rhs) {
+			num := aoc.ParseInt(field)
+			if !seen.Add(num) {
+				winning.Add(num)
+			}
 		}
 
-		var numbers aoc.Set[int]
-		for !fields.Empty() {
-			numbers.Add(aoc.ParseInt(fields.PopFront()))
-		}
-
-		return Card{ID: id, Winning: winning, Numbers: numbers}, nil
+		return len(winning), nil
 	})
 }
