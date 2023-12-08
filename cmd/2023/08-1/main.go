4d5929d7d5e3f2518ed9a42ee518834f14789a79
Brandon Beck <bmbeck@gmail.com>
2023-12-07 23:34:18 -0600

    2023/08

---
+++ cmd/2023/08-1/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/bbeck/advent-of-code/aoc"
+)
+
+func main() {
+	instructions, root := InputToInstructionsAndGraph()
+
+	var n int
+	for n = 0; root.ID != "ZZZ"; n++ {
+		instruction := instructions[n%len(instructions)]
+		if instruction == 'L' {
+			root = root.Left
+		} else {
+			root = root.Right
+		}
+	}
+
+	fmt.Println(n)
+}
+
+type Node struct {
+	ID    string
+	Left  *Node
+	Right *Node
+}
+
+func InputToInstructionsAndGraph() (string, *Node) {
+	lines := aoc.InputToLines(2023, 8)
+	instructions := lines[0]
+
+	nodes := make(map[string]*Node)
+	get := func(id string) *Node {
+		if node := nodes[id]; node == nil {
+			nodes[id] = &Node{ID: id}
+		}
+		return nodes[id]
+	}
+
+	for _, line := range lines[2:] {
+		line = strings.ReplaceAll(line, "=", "")
+		line = strings.ReplaceAll(line, "(", "")
+		line = strings.ReplaceAll(line, ",", "")
+		line = strings.ReplaceAll(line, ")", "")
+		fields := strings.Fields(line)
+
+		node := get(fields[0])
+		node.Left = get(fields[1])
+		node.Right = get(fields[2])
+	}
+
+	return instructions, nodes["AAA"]
+}
---
+++ cmd/2023/08-2/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/bbeck/advent-of-code/aoc"
+)
+
+func main() {
+	instructions, nodes := InputToInstructionsAndGraph()
+
+	lcm := 1
+	for _, node := range nodes {
+		if strings.HasSuffix(node.ID, "A") {
+			lcm = aoc.LCM(lcm, Length(instructions, node))
+		}
+	}
+
+	fmt.Println(lcm)
+}
+
+func Length(instructions string, root *Node) int {
+	var n int
+	for n = 0; !strings.HasSuffix(root.ID, "Z"); n++ {
+		instruction := instructions[n%len(instructions)]
+		if instruction == 'L' {
+			root = root.Left
+		} else {
+			root = root.Right
+		}
+	}
+
+	return n
+}
+
+type Node struct {
+	ID    string
+	Left  *Node
+	Right *Node
+}
+
+func InputToInstructionsAndGraph() (string, []*Node) {
+	lines := aoc.InputToLines(2023, 8)
+	instructions := lines[0]
+
+	nodes := make(map[string]*Node)
+	get := func(id string) *Node {
+		if node := nodes[id]; node == nil {
+			nodes[id] = &Node{ID: id}
+		}
+		return nodes[id]
+	}
+
+	for _, line := range lines[2:] {
+		line = strings.ReplaceAll(line, "=", "")
+		line = strings.ReplaceAll(line, "(", "")
+		line = strings.ReplaceAll(line, ",", "")
+		line = strings.ReplaceAll(line, ")", "")
+		fields := strings.Fields(line)
+
+		node := get(fields[0])
+		node.Left = get(fields[1])
+		node.Right = get(fields[2])
+	}
+
+	return instructions, aoc.GetMapValues(nodes)
+}
