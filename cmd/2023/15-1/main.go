b9668f0194bfa3a46eba71637012b7791905a2ba
Brandon Beck <bmbeck@gmail.com>
2023-12-14 23:51:12 -0600

    2023/15

---
+++ cmd/2023/15-1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bbeck/advent-of-code/aoc"
+	"strings"
+)
+
+func main() {
+	line := aoc.InputToString(2023, 15)
+
+	var sum int
+	for _, field := range strings.Split(line, ",") {
+		sum += Hash(field)
+	}
+	fmt.Println(sum)
+}
+
+func Hash(s string) int {
+	var hash int32
+	for _, c := range s {
+		hash = 17 * (hash + c) % 256
+	}
+	return int(hash)
+}
---
+++ cmd/2023/15-2/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bbeck/advent-of-code/aoc"
+	"strings"
+)
+
+func main() {
+	var boxes [256][]Lens
+
+	find := func(box int, label string) (int, bool) {
+		for i, lens := range boxes[box] {
+			if lens.Label == label {
+				return i, true
+			}
+		}
+
+		return 0, false
+	}
+
+	for _, op := range InputToOperations() {
+		box := op.Hash()
+		index, found := find(box, op.Label)
+
+		switch op.Kind {
+		case "-":
+			if found {
+				boxes[box] = append(boxes[box][:index], boxes[box][index+1:]...)
+			}
+
+		case "=":
+			lens := Lens{Label: op.Label, FocalLength: op.FocalLength}
+
+			if found {
+				boxes[box][index] = lens
+			} else {
+				boxes[box] = append(boxes[box], lens)
+			}
+		}
+	}
+
+	var power int
+	for b, box := range boxes {
+		for l, lens := range box {
+			power += (b + 1) * (l + 1) * lens.FocalLength
+		}
+	}
+	fmt.Println(power)
+}
+
+type Lens struct {
+	Label       string
+	FocalLength int
+}
+
+type Operation struct {
+	Kind        string
+	Label       string
+	FocalLength int
+}
+
+func (o Operation) Hash() int {
+	var hash int32
+	for _, c := range o.Label {
+		hash = 17 * (hash + c) % 256
+	}
+	return int(hash)
+}
+
+func InputToOperations() []Operation {
+	line := aoc.InputToString(2023, 15)
+	parts := strings.Split(line, ",")
+
+	var ops []Operation
+	for _, part := range parts {
+		var op Operation
+		if label, _, ok := strings.Cut(part, "-"); ok {
+			op.Kind = "-"
+			op.Label = label
+		}
+
+		if label, length, ok := strings.Cut(part, "="); ok {
+			op.Kind = "="
+			op.Label = label
+			op.FocalLength = aoc.ParseInt(length)
+		}
+
+		ops = append(ops, op)
+	}
+
+	return ops
+}
