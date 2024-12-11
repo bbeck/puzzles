ed34e2d6ec8f467dabf4de4d14ed8a01cf0faba0
Brandon Beck <bmbeck@gmail.com>
2024-12-10 21:57:00 -0600

    Optimized Advent of Code: 2024-06

--- cmd/advent-of-code/2024/06-2/main.go
+++ cmd/advent-of-code/2024/06-2/main.go
@@ -13,53 +13,69 @@ func main() {
 		if s == "^" {
 			guard.Location = p
 			guard.Heading = Up
+			grid.SetPoint(p, ".")
 		}
 	})
 
 	// Figure out the guard's initial path without any additional obstacles.
 	// Since these are the only locations the guard will visit, they are the only
 	// places where we need to consider adding an obstacle.
-	candidates, _ := Walk(grid, guard)
+	path, _ := Walk(grid, guard)
 
-	var ps Set[Point2D]
-	for p := range candidates {
-		if grid.GetPoint(p) == "." && IsLoop(grid, guard, p) {
-			ps.Add(p)
+	var tried, looped Set[Point2D]
+	for i := 1; i < len(path); i++ {
+		// Don't place an obstacle at the same location twice.  Since the same
+		// location came up again it means that the path already went through this
+		// location.  Thus, it must remain open so that we could even get here the
+		// second time.
+		if !tried.Add(path[i].Location) {
+			continue
+		}
+
+		grid.SetPoint(path[i].Location, "#")
+		_, isLoop := Walk(grid, path[i-1])
+		grid.SetPoint(path[i].Location, ".")
+
+		if isLoop {
+			looped.Add(path[i].Location)
 		}
 	}
-	fmt.Println(len(ps))
+
+	fmt.Println(len(looped))
 }
 
-func Walk(grid Grid2D[string], guard Turtle) (Set[Point2D], bool) {
+func Walk(grid Grid2D[string], guard Turtle) ([]Turtle, bool) {
+	var path []Turtle
 	var seen Set[Turtle]
 	for {
+		if !grid.InBoundsPoint(guard.Location) {
+			return path, false
+		}
+
 		if !seen.Add(guard) {
 			return nil, true
 		}
 
-		mark := guard
-		guard.Forward(1)
-		if !grid.InBoundsPoint(guard.Location) {
+		for {
+			_, s, _ := PeekForward(grid, guard)
+			if s == "#" {
+				guard.TurnRight()
+				continue
+			}
+
 			break
 		}
 
-		if s := grid.GetPoint(guard.Location); s != "." && s != "^" {
-			guard = mark
-			guard.TurnRight()
-		}
+		path = append(path, guard)
+		guard.Forward(1)
 	}
-
-	var pts Set[Point2D]
-	for t := range seen {
-		pts.Add(t.Location)
-	}
-	return pts, false
 }
 
-func IsLoop(grid Grid2D[string], guard Turtle, obstacle Point2D) bool {
-	grid.SetPoint(obstacle, "#")
-	defer grid.SetPoint(obstacle, ".")
+func PeekForward(grid Grid2D[string], t Turtle) (Point2D, string, bool) {
+	t.Forward(1)
 
-	_, loop := Walk(grid, guard)
-	return loop
+	if !grid.InBoundsPoint(t.Location) {
+		return Origin2D, "", false
+	}
+	return t.Location, grid.GetPoint(t.Location), true
 }
