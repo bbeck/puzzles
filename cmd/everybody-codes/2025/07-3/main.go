94bcea0b8369c55cfe81b46430ef9944d5f5e1ee
Brandon Beck <bmbeck@gmail.com>
2025-11-11 21:47:11 -0600

    Everybody Codes: Improve performance of 2025-07-3

--- cmd/everybody-codes/2025/07-3/main.go
+++ cmd/everybody-codes/2025/07-3/main.go
@@ -10,37 +10,74 @@ import (
 func main() {
 	names, rules := InputToNamesAndRules()
 
-	var seen Set[string]
+	// Generate all possible 6 character names by growing the valid input names
+	// up to length 6.
+	names = Generate6(names, rules)
+
+	// Now consider each rule letter as the last letter of a 6 character name.
+	// Compute how many valid suffixes can be appended to it to form a name
+	// between 7 and 11 characters long.
+	counts := CountSuffixes(rules)
+
+	var sum int
 	for _, name := range names {
-		if IsValid(name, rules) {
-			Visit(name, rules, &seen)
-		}
+		sum += counts[name[len(name)-1:]]
 	}
-	fmt.Println(len(seen))
+	fmt.Println(sum)
 }
 
-func Visit(name string, rules map[string]Set[string], seen *Set[string]) {
-	if 7 <= len(name) && len(name) <= 11 {
-		if !seen.Add(name) {
+func Generate6(names []string, rules map[string]Set[string]) []string {
+	var seen Set[string]
+
+	var valid = func(name string) bool {
+		for i := 0; i < len(name)-1; i++ {
+			current, next := name[i:i+1], name[i+1:i+2]
+			if !rules[current].Contains(next) {
+				return false
+			}
+		}
+		return true
+	}
+
+	var visit func(name string)
+	visit = func(name string) {
+		if len(name) == 6 {
+			seen.Add(name)
 			return
 		}
-	}
 
-	if len(name) < 11 {
 		for suffix := range rules[name[len(name)-1:]] {
-			Visit(name+suffix, rules, seen)
+			visit(name + suffix)
 		}
 	}
+
+	for _, name := range names {
+		if valid(name) {
+			visit(name)
+		}
+	}
+	return seen.Entries()
 }
 
-func IsValid(name string, rules map[string]Set[string]) bool {
-	for i := 0; i < len(name)-1; i++ {
-		current, next := name[i:i+1], name[i+1:i+2]
-		if !rules[current].Contains(next) {
-			return false
+func CountSuffixes(rules map[string]Set[string]) map[string]int {
+	var count func(letter string, length int) int
+	count = func(letter string, length int) int {
+		if length == 0 {
+			return 0
 		}
+
+		var sum int
+		for suffix := range rules[letter] {
+			sum += 1 + count(suffix, length-1)
+		}
+		return sum
 	}
-	return true
+
+	result := make(map[string]int)
+	for letter := range rules {
+		result[letter] = count(letter, 5)
+	}
+	return result
 }
 
 func InputToNamesAndRules() ([]string, map[string]Set[string]) {
