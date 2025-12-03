64f221e75555317b94b907d0d1d46d76b8914fff
Brandon Beck <bmbeck@gmail.com>
2025-12-02 23:57:43 -0600

    Advent of Code: 2025-01

---
+++ cmd/advent-of-code/2025/01-1/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	. "github.com/bbeck/puzzles/lib"
+	"github.com/bbeck/puzzles/lib/in"
+)
+
+var Deltas = map[byte]int{'L': -1, 'R': +1}
+
+func main() {
+	var count int
+
+	var dial = 50
+	for in.HasNext() {
+		line := in.Line()
+		step, num := Deltas[line[0]], ParseInt(line[1:])
+
+		dial = Modulo(dial+step*num, 100)
+		if dial == 0 {
+			count++
+		}
+	}
+
+	fmt.Println(count)
+}
